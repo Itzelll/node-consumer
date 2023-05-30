fb3f1d5ce2dc9ea43aec4affcdc7ed45dc72f523
itzelll <mmitzel2435@gmail.com>
2023-05-29 22:38:30 -0600

    topics reactions-comments

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const kafka = new Kafka({
   clientId: 'my-app',
   brokers: [
     'comida-kafka-0.comida-kafka-headless.itzel2-itzelll.svc.cluster.local:9092'
-//    'localhost:9092',
-//	  'my-kafka-0.my-kafka-headless.kafka-adsoftsito.svc.cluster.local:9092'
-	  ]
+    //    'localhost:9092',
+    //	  'my-kafka-0.my-kafka-headless.kafka-adsoftsito.svc.cluster.local:9092'
+  ]
 });
 
 const producer = kafka.producer()
@@ -25,26 +25,72 @@ app.get('/', (req, res, next) => {
 
 const run = async (username) => {
 
-    await producer.connect()
-//    await producer.send()
-    await producer.send({
-      topic: 'test',
-      messages: [ 
-	{ 
-	  'value': `{"name": "${username}" }` 
-  	} 
-      ],
-    })
-   await producer.disconnect()
+  await producer.connect()
+  //    await producer.send()
+  await producer.send({
+    topic: 'test',
+    messages: [
+      {
+        'value': `{"name": "${username}" }`
+      }
+    ],
+  })
+  await producer.disconnect()
 }
 
 app.get('/like', (req, res, next) => {
   const username = req.query.name;
-  res.send({ 'name' : username } );
+  res.send({ 'name': username });
   run(username).catch(e => console.error(`[example/producer] ${e.message}`, e))
 
 });
 
-app.listen(port,  () => 
-	console.log('listening on port ' + port
-));
+//node - topic reaction 
+const reaction = async (uId, oId, rId) => {
+  await producer.connect()
+  await producer.send({
+    topic: 'reactions',
+    messages: [
+      {
+        'value': `{"userId": "${uId}", "objectId": "${oId}", "reaction": "${rId}"}`
+      }
+    ],
+  })
+  await producer.disconnect()
+}
+
+app.get('/reaction', (req, res, next) => {
+  const uId = req.query.userId;
+  const oId = req.query.objectId;
+  const rId = req.query.reactionId;
+  res.send({ 'userId': uId, 'objectId': oId, 'reactionId': rId });
+  reaction(uId, oId, rId).catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+});
+
+//node - topic comments    uId, oId, message
+const comments = async (uId, oId, comment) => {
+  await producer.connect()
+  await producer.send({
+    topic: 'comments',
+    messages: [
+      {
+        'value': `{"userId": "${uId}", "objectId": "${oId}", "comment": "${comment}"}`
+      }
+    ],
+  })
+  await producer.disconnect()
+}
+
+app.get('/comments', (req, res, next) => {
+  const uId = req.query.userId;
+  const oId = req.query.objectId;
+  const comment = req.query.comment;
+  res.send({ 'userId': uId, 'objectId': oId, 'comment': comment });
+  reaction(uId, oId, comment).catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+});
+
+app.listen(port, () =>
+  console.log('listening on port ' + port
+  ));
